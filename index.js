d93346eb49b39a2af402b85143296b08dbdf3894
Obaidullah-ahsan <abir60303@gmail.com>
2024-05-12 02:20:23 +0600

    delete assignment spacific id

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentsCollection.deleteOne(query);
+      res.send(result);
+    });
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
