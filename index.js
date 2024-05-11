ca4f3ce478b7421a940bf0312be98d2964416afd
Obaidullah-ahsan <abir60303@gmail.com>
2024-05-12 01:37:24 +0600

    get all assignments data

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkk0rbw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,6 +23,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const assignmentsCollection = client
+      .db("assignmentsDB")
+      .collection("assignments");
+
+    app.post("/assignments", async (req, res) => {
+      const newAssignment = req.body;
+      const result = await assignmentsCollection.insertOne(newAssignment);
+      res.send(result);
+    });
+
+    app.get("/assignments", async (req, res) => {
+      const cursor = assignmentsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
