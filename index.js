77b0aeb7cd016bb9c0120976c883a40115a7fb89
Obaidullah-ahsan <abir60303@gmail.com>
2024-05-12 20:57:47 +0600

    get a single assignment

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/assignment/:id", async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await assignmentsCollection.findOne(query)
+      res.send(result)
+    })
+
     app.delete("/assignment/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
